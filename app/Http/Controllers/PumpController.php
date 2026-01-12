16101ec198aa8a7dcaae946f3c1e19df8f3214f3
asanihasan <asanihasan999@gmail.com>
2026-01-12 15:43:08 +0700

    update controller

--- app/Http/Controllers/PumpController.php
+++ app/Http/Controllers/PumpController.php
@@ -6,6 +6,7 @@ use App\Models\Pump;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http; // Import HTTP Client
+use Carbon\Carbon;
 
 class PumpController extends Controller
 {
