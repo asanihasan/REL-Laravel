c84cd9d23275c3a9615ecdf317741b2bbb2656c2
asanihasan <82603969+asanihasan@users.noreply.github.com>
2026-06-19 14:26:13 +0700

    Update user.blade.php

--- resources/views/manage/user.blade.php
+++ resources/views/manage/user.blade.php
@@ -135,12 +135,14 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z" /><path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z" /></svg>
                                 </button>
                                 
+                                @if($group->id != 1)
                                 <form action="/user-groups/{{ $group->id }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this group?');">
                                     @csrf @method('DELETE')
                                     <button type="submit" class="p-2 bg-red-50 text-red-600 hover:bg-red-100 rounded transition" title="Delete">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path fill-rule="evenodd" d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z" clip-rule="evenodd" /></svg>
                                     </button>
                                 </form>
+                                @endif
                             </div>
                         </td>
                     </tr>
@@ -216,7 +218,7 @@
                 <input type="text" name="name" id="modalGroupName" class="w-full border border-gray-300 p-2 rounded focus:ring-red-500 focus:border-red-500" required>
             </div>
             
-            <div class="space-y-3 mb-6 bg-gray-50 p-4 rounded border">
+            <div class="space-y-3 mb-6 bg-gray-50 p-4 rounded border" id="groupCheckboxesWrapper">
                 <label class="flex items-center space-x-2 cursor-pointer">
                     <input type="checkbox" name="view" id="modalGroupView" class="rounded text-red-600 focus:ring-red-500">
                     <span class="text-sm text-gray-700 font-medium">View</span>
@@ -319,19 +321,27 @@
             $('#groupModalTitle').text('Edit Group');
             $('#groupForm').attr('action', '/user-groups/' + group.id);
             $('#groupFormMethod').val('PUT');
-            
             $('#modalGroupName').val(group.name);
-            $('#modalGroupView').prop('checked', group.view);
-            $('#modalGroupControl').prop('checked', group.control);
-            $('#modalGroupHistorical').prop('checked', group.historical);
-            $('#modalGroupDataManager').prop('checked', group.data_manager);
+            
+            // Hide checkboxes if ID is 1
+            if (group.id == 1) {
+                $('#groupCheckboxesWrapper').addClass('hidden');
+            } else {
+                $('#groupCheckboxesWrapper').removeClass('hidden');
+                $('#modalGroupView').prop('checked', group.view);
+                $('#modalGroupControl').prop('checked', group.control);
+                $('#modalGroupHistorical').prop('checked', group.historical);
+                $('#modalGroupDataManager').prop('checked', group.data_manager);
+            }
         } else {
             $('#groupModalTitle').text('Add Group');
             $('#groupForm').attr('action', '/user-groups');
             $('#groupFormMethod').val('POST');
+            $('#groupCheckboxesWrapper').removeClass('hidden'); // Ensure visible for adding
         }
         $('#groupModal').removeClass('hidden');
     }
+
 
     // --- Telegram Form Logic ---
     function openTelegramModal(id) {
