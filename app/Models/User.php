eabc4e740b8a9b92dbecf0573bdec568ee68134a
asanihasan <asanihasan999@gmail.com>
2026-06-20 00:56:29 +0700

    fix telegram webhook

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'password',
         'user_group_id',
         'telegram_id',
+        'telegram_link_token',
         'engine_running',
         'engine_stopped',
         'high_rpm',
