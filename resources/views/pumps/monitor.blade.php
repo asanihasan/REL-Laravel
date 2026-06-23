e392a169bdcd8ce944880c0d6123a948f6c765e4
asanihasan <asanihasan999@gmail.com>
2026-06-23 13:09:03 +0700

    update graphical view

--- resources/views/pumps/monitor.blade.php
+++ resources/views/pumps/monitor.blade.php
@@ -470,7 +470,7 @@
                     updateGauge('gauge_engine_temp', data.engine_temp_mech);
                     updateGauge('gauge_coolant_temp', data.coolant_temp);
                     updateGauge('gauge_fuel_level', data.fuel_level);
-                    console.log(data)
+                    console.log(JSON.parse("{{ json_encode($pump->location) }}"))
                 }
             });
         }, 1000);
